0c922a21b8e5d2661a7c9a7aac7d175d5e30c61d
welingtoncb <welingtoncb@yahoo.com.br>
2019-09-30 09:15:24 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,19 @@
 <?php
 
-$this->group(['middleware' => ['auth']],function(){
+Auth::routes();
+
+$this->group(['middleware' => ['auth']], function() {
     
+    $this->get('/', 'HomeController@index');
+
     $this->get('/home', 'HomeController@index')->name('home');
-    
-    $this->post('/imovel','ImovelController@create')->name('imovel');
-    $this->get('/imovel','ImovelController@create')->name('imovel');
-    $this->post('/store','ImovelController@store')->name('store');
+    $this->match(['get', 'post'], '/imovel', 'ImovelController@create')->name('imovel');
+    $this->post('/store/{source}','ImovelController@store')->name('store');
     $this->get('/listagem','ImovelController@listing')->name('listing');
     $this->get('/imovel/{id}/edit','ImovelController@edit')->name('edit');
     $this->put('/update/{id}','ImovelController@update')->name('update');
     $this->any('/delete/{id}','ImovelController@delete')->name('delete');
+    $this->get('/view/{id}','ImovelController@view')->name('view');
     $this->any('/pesquisa','ImovelController@searchImmobile')->name('searchImmobile');
+    $this->match(['get', 'post'], '/import','ImovelController@createImportXML')->name('import');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
